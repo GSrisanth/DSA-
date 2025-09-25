5d9d442b2e4350c3ff9c1e9393e61d2f55bcfcd7
GSrisanth <srisanthchowdary@gmail.com>
2025-09-25 13:06:04 +0530

    Trees

---
+++ Node.java
@@ -0,0 +1,27 @@
+
+// Representation of trees in java
+
+
+// public class Node {
+//     int data;
+//     Node left;
+//     Node right;
+//     public Node(int key){
+//         data = key;
+//     }
+//     public static void main(String[] args) {
+//     Node root = new Node(1);
+//     root.left = new Node(2);
+//     root.right = new Node(3);
+//     root.right.left = new Node(4);
+
+//     System.out.println("Binary tree created");
+//     System.out.println("Root data: " + root.data);
+//     System.out.println("Left child of root: " + root.left.data);
+//     System.out.println("Right child of root: " + root.right.data);
+//     System.out.println("Left child of right child of root: " + root.right.left.data);
+
+//     }
+// }    
+
+
